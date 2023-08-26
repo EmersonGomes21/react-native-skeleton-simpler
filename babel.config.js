3e99c9367b6843a8587fe70627871e4bb44d6468
Emerson Gomes Rebolsas <emerson@avamobile.com.br>
2023-08-26 14:27:18 -0300

    feat: add configs of build

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+
+
+  module.exports = {
+    presets: ['module:metro-react-native-babel-preset', '@babel/preset-react','@babel/preset-typescript'],
+    ignore: ["./lib", "node_modules"]
+  
+};
+
+  
